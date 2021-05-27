f79e9f41caba13bdac4fc7d6a2f9f856a5102f33
unknown <ksagun@abckidsny.com>
2021-05-27 12:16:10 +0800

    ffs/module-1-add-on-submit-preloader: Updated backend api endpoint in prod

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,10 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
 };
 
-export const baseUrl = `http://localhost/fleefood/api/`;
+export const baseUrl = `http://api.fleefood.com/`;
 
 /*
  * For easier debugging in development mode, you can import the following file
